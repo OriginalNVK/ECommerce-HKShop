98489bec590cea10696da89dff769a4b079b16ce
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-02-15 09:15:16 +0700

    Add HangHoa Controller and MenuLoai ViewComponent

---
+++ HShop/HShop/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HShop.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index(int? loai)
+        {
+            return View();
+        }
+    }
+}
