cda05f3ef77c3b3c4fd4113ddf0f4330d5caf15d
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-03-26 02:46:15 +0700

    Change UI of the website

---
+++ HShop/HShop/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using HShop.Data;
+using HShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HShop.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+        public CategoryViewComponent(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(l => new MenuLoaiVM
+            {
+                MaLoai = l.MaLoai,
+                TenLoai = l.TenLoai,
+                SoLuong = l.HangHoas.Count
+            }).OrderBy(p => p.TenLoai);
+
+            return View(data);
+        }
+    }
+}
--- HShop/HShop/obj/Debug/net8.0/HShop.AssemblyInfo.cs
+++ HShop/HShop/obj/Debug/net8.0/HShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b13e35e5e1f377057a4f033f7c9297fd967cccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f8b74bf111844985bc98837e7179809d05c3ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
